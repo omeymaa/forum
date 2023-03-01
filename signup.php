ed6236ca9f735fd69d87d76ebf045355bac21959
omeymaa <omeyma.maatoug@gmail.com>
2023-03-01 21:35:11 +0100

    premier commit

---
+++ signup.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'includes/head.php'; ?>
+
+<body>
+    <form class="container">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Pseudo</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Prénom</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nom</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">S'inscrire</button>
+    </form>
+</body>
+
+</html>
