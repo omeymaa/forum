c59efe1359c85ce49ab53f3b966835da124a71a9
omeymaa <omeyma.maatoug@gmail.com>
2023-03-02 09:36:39 +0100

    commit

--- signup.php
+++ signup.php
@@ -1,30 +1,34 @@
+<?php
+    require('actions/signupAction.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'includes/head.php'; ?>
 
 <body>
-    <form class="container">
+    <form class="container" method="POST">
+        <?php  
+            if(isset($errorMsg)) { echo '<p>'.$errorMsg.'</p>'; }
+        ?>
+
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Pseudo</label>
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" name="pseudo">
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Prénom</label>
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" name="firstname">
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Nom</label>
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" name="lastname">
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <input type="password" class="form-control" name="password">
         </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">S'inscrire</button>
+        <button type="submit" class="btn btn-primary" name="validate">S'inscrire</button>
     </form>
 </body>
 
